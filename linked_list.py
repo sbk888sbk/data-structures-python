c59589b65a9575a22a03008b017e6dd133faeec1
Sai Bala Krishna Allamsetty <sbk888.sbk@gmail.com>
2018-11-22 03:54:38 -0500

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -42,6 +42,24 @@ class Linked :
 			except:
 				if(temp.next==None):
 					print("No Element Found")
+	
+	def delete_index(self,index):
+		current_index=0;
+		temp=self.root
+		if(index==0):
+			print("Deleted Root Element "+str(temp.value))
+			self.root=temp.next
+			print("New root element is : "+str(self.root.value))
+			return
+		while(current_index+1!= index and temp.next is not None):
+			temp=temp.next
+			current_index+=1
+		if(temp.next is None):
+			print("Index: "+str(index)+" is greater than linked list length")
+			return
+		print("Deleted Element at Index: "+str(index)+" Node: "+str(temp.next.value))
+		temp.next=temp.next.next
+		
 
 ll=Linked()
 ll.insert(2)
@@ -50,4 +68,7 @@ ll.insert(5)
 ll.insert(6)
 ll.insert(7)
 ll.delete(10)
+ll.display()
+ll.delete_index(2)
+ll.delete_index(10)
 ll.display()
